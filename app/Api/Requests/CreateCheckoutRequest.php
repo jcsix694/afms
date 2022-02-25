c092c4b4a209c975cc3938acf8efad1a6e7dff31
jcsix694 <jcsix694@gmail.com>
2022-02-25 19:40:06 +0000

    ADDED checkout request

---
+++ app/Api/Requests/CreateCheckoutRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Api\Requests;
+
+use App\Api\Models\CheckoutModel;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCheckoutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'amount' => 'required|regex:/^\d*(\.\d{2})?$/|numeric',
+            'reference' => 'required|string|min:8|max:255|unique:'. CheckoutModel::class .',reference',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'amount.regex' => 'Incorrect format, must be currency with 2 decimal places'
+        ];
+    }
+}
